f025322f429b354353279e5224802d286b777dc8
Felix Pfannkuch <felix@pfannkuchs.de>
2023-11-09 22:17:50 +0100

    Committed, just in case of apocalypse. More CP work in progress, than real code

--- src/Channel/MatrixChannel.php
+++ src/Channel/MatrixChannel.php
@@ -34,12 +34,114 @@ class MatrixChannel extends ChannelBase
     {
         return true;
     }
+
     /**
      * Summary of doStuff
      * @return int
      */
     protected /*abstractImpl*/function doStuff(): int
     {
+        $message = $this->getUnprocessedMessage();
+
+        if ($message == null)
+            return 0; // 0 Nachrichten verarbeitet
+
+        try
+        {
+            $mediaId = null;
+
+            // Wenn ein Bild vorhanden ist, muss zuerst das Bild hochgeladen werden
+            if ($message->hasImage())
+            {
+                $filename = basename(parse_url($message->getMetadata()['imageUrl'] ?? 'unknown.jpg', PHP_URL_PATH));
+                $mimetype = $message->getMimeType();
+
+                $delimiter = '-------------' . uniqid();
+
+                $options = array(
+                    'http' => array(
+                        'header' => array(
+                            'Accept: application/json',
+                            'User-Agent: DARC NewsRouter MastodonChannel v1.0',
+                            "Authorization: Bearer " . $this->getParameter(self::PARAM_TOKEN),
+                            "Content-Type: multipart/form-data; boundary=$delimiter",
+                        ),
+                        'method' => 'POST',
+                        'content' => "--$delimiter\r\n"
+                            . "Content-Disposition: form-data; name=\"file\"; filename=\"{$filename}\"\r\n"
+                            . "Content-Type: {$mimetype}\r\n"
+                            . "\r\n"
+                            . $message->getImageData()
+                            . "\r\n"
+                            . "--$delimiter--",
+                    ),
+                );
+
+                $url = $this->GetParameter(self::PARAM_SERVERBASEURL) . '/api/v2/media';
+                $resultText = file_get_contents($url, false, stream_context_create($options)); // send https request
+
+                if ($resultText === false)
+                {
+                    throw new \ErrorException("Fehler beim Aufruf der Mastodon-API für Bildupload: {$http_response_header[0]}");
+                }
+
+                $result = json_decode($resultText, true); // decode JSON
+                $mediaId = $result['id'];
+            }
+
+            // post Status
+            $options = array(
+                'http' => array(
+                    'protocol_version' => '1.1',
+                    'method' => 'POST',
+                    'header' => array(
+                        'Content-Type: application/json; charset=utf-8',
+                        'Accept: application/json',
+                        'User-Agent: DARC NewsRouter MastodonChannel v1.0',
+                    ),
+                    'content' => json_encode(
+                        [
+                            'media_ids' => [$mediaId],
+                            'spoiler_text' => $message->getTitel() . ($message->hasTeaser() ? ("\n" . $message->getTeaser() . "\n") : null),
+                            'status' => $message->getText(),
+                            'visibility' => 'public',
+                            'language' => 'de'
+                        ]
+                    )
+                )
+            );
+
+            $url = $this->GetParameter(self::PARAM_SERVERBASEURL) . '/api/v1/statuses';
+            $resultText = file_get_contents($url, false, stream_context_create($options)); // send https request
+
+            if ($resultText === false)
+            {
+                throw new \ErrorException("Fehler beim Aufruf der Telegram-API: {$http_response_header[0]}");
+            }
+
+
+            $result = json_decode($resultText, true); // decode JSON
+            $uniqueId = $result['id'];
+            $this->setMessageProcessed($message, true, $uniqueId);
+
+            return 1; // eine Nachricht verarbeitet
+        }
+        catch (\Exception $ex)
+        {
+            $this->setMessageProcessed($message, false, null);
+            Logger::Error(static::class . " ({$this->getName()}): Fehler beim Versand von Nachricht {$message->getId()} über Channel {$this->getId()}\n");
+            Logger::Error($ex->getMessage());
+
+            return ErrorCodes::Operation_Failed;
+        }
+    }
+    /**
+     * Summary of doStuffold
+     * @return int
+     */
+    protected /*abstractImpl*/function doStuffold(): int
+    {
+
         $message = $this->getUnprocessedMessage();
         $text = strval($message->getText());
         Logger::Error($text);
